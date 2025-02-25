941836fd39adce775bebc2816043c63bbde31b26
Programador <vtex@subeagenciadigital.com>
2025-02-25 14:01:31 +0100

    Config name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home: React.FC = () => {
   return (
     <header>
-      <h1>Hi, I'm [Your Name]</h1>
+      <h1>Hi, I'm Aymara</h1>
       <p>Frontend Developer passionate about creating beautiful and functional user interfaces with React and modern web technologies.</p>
       <button><Link to='/projects'>Explore my projects</Link></button>
     </header>
